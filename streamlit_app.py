5bc5af0ab6d0642ac544ab9acbc097b6c79ecfe1
Rayan Alshehri <116816027+ryyaon14@users.noreply.github.com>
2024-06-11 20:56:53 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,17 +11,17 @@ This app predicts the Iris flower type!
 
 st.sidebar.header('User Input Parameters')
 
-def user_input_features():
-    sepal_length = st.sidebar.slider('Sepal Length', 4.3, 7.9, 5.4)
-    sepal_width = st.sidebar.slider('Sepal Width', 2.0, 4.4, 3.4)
-    petal_length = st.sidebar.slider('Petal Length', 1.0, 6.9, 1.3)
-    petal_width = st.sidebar.slider('Petal Width', 0.1, 2.5, 0.2)
-    data = {'sepal length': sepal_length,
-            'sepal width': sepal_width,
-            'petal length': petal_length,
-            'petal width': petal_width}
-    features = pd.DataFrame(data, index=[0])
-    return features
+# def user_input_features():
+#     sepal_length = st.sidebar.slider('Sepal Length', 4.3, 7.9, 5.4)
+#     sepal_width = st.sidebar.slider('Sepal Width', 2.0, 4.4, 3.4)
+#     petal_length = st.sidebar.slider('Petal Length', 1.0, 6.9, 1.3)
+#     petal_width = st.sidebar.slider('Petal Width', 0.1, 2.5, 0.2)
+#     data = {'sepal length': sepal_length,
+#             'sepal width': sepal_width,
+#             'petal length': petal_length,
+#             'petal width': petal_width}
+#     features = pd.DataFrame(data, index=[0])
+#     return features
 
 # df = user_input_features()
 
