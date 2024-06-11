85056a27ca3809652d1040fba374e2412c957bf4
Rayan Alshehri <116816027+ryyaon14@users.noreply.github.com>
2024-06-11 20:56:27 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,7 +23,7 @@ def user_input_features():
     features = pd.DataFrame(data, index=[0])
     return features
 
-df = user_input_features()
+# df = user_input_features()
 
 # st.subheader('User Input Parameters')
 # st.write(df)
