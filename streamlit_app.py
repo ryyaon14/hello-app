9d9575ba54f219be64d4eb5a3603f931b11d32d0
Rayan Alshehri <116816027+ryyaon14@users.noreply.github.com>
2024-06-11 20:53:51 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,49 +3,46 @@ import pandas as pd
 from sklearn import datasets
 from sklearn.ensemble import RandomForestClassifier
 
-st.write('hello world!')
+st.write('Hello, world!')
 st.write("""
-simple iris flower prediction app
-this app predicts the oris flower type!
+# Simple Iris Flower Prediction App
+This app predicts the Iris flower type!
 """)
 
-
-
-
-st.sidebar.header('user input parameters')
+st.sidebar.header('User Input Parameters')
 
 def user_input_features():
-  sepal_length= st.sidebar.slider('sepal length' , 4.3 , 7.9,5.4)
-  sepal_width= st.sidebar.slider('sepal width' , 2.0 , 4.4 , 3.4)
-  petal_length= st.sidebar.slider('petal length' , 1.0, 6.9 , 1.3)
-  petal_width = st.sidebar.slider('petal width' , 0.1 , 2.5 , 0.2)
-  data = { 'sepal length' : sepal_length,
-           'sepal width' : sepal_width,
-           'petal length' :  petal_length,
-            'petal width' : petal_width}
-  features = pd.DataFrame(data, index=[0])
-  return features
-  
+    sepal_length = st.sidebar.slider('Sepal Length', 4.3, 7.9, 5.4)
+    sepal_width = st.sidebar.slider('Sepal Width', 2.0, 4.4, 3.4)
+    petal_length = st.sidebar.slider('Petal Length', 1.0, 6.9, 1.3)
+    petal_width = st.sidebar.slider('Petal Width', 0.1, 2.5, 0.2)
+    data = {'sepal length': sepal_length,
+            'sepal width': sepal_width,
+            'petal length': petal_length,
+            'petal width': petal_width}
+    features = pd.DataFrame(data, index=[0])
+    return features
+
 df = user_input_features()
 
-st.subheader('User input parameters')
+st.subheader('User Input Parameters')
 st.write(df)
 
-iris=datasets.load_iris()
-X= iris.data
-Y=iris.target
+iris = datasets.load_iris()
+X = iris.data
+Y = iris.target
 
-clf=RandomForestClassifier()
-clf.fit(X,Y)
+clf = RandomForestClassifier()
+clf.fit(X, Y)
 
-prediction=clf.predict(df)
-prediction_proba=clf.predict_proba(df)
+prediction = clf.predict(df)
+prediction_proba = clf.predict_proba(df)
 
-st.subheader('Class lables and thier corresponding index number')
+st.subheader('Class Labels and their Corresponding Index Number')
 st.write(iris.target_names)
 
-st.subheader('prediction')
-st.write(iris.target_names[prediction])
+st.subheader('Prediction')
+st.write(iris.target_names[prediction][0])
 
-st.subheader('prediction_probapilty')
-st.write(iris.target_names[prediction_proba])
+st.subheader('Prediction Probability')
+st.write(prediction_proba)
